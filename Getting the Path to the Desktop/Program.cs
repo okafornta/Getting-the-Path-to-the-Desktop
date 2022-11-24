b0d540b353d408704d435ef32a25a19b3277569e
Okafornta <hillaryonia@gmail.com>
2022-11-24 12:22:37 +0100

    Add project files.

---
+++ Getting the Path to the Desktop/Program.cs
@@ -0,0 +1,26 @@
+﻿/* TASK: 
+ * 
+ *  When you work with files, you might want to create a file on the user’s desktop. However,
+    everybody has their own file system path to the desktop. I will show you how to find that
+    path
+ */
+// Finding path to the desktop
+string pathToDeskTop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+//Output
+Console.WriteLine("Way to my desktop - " + pathToDeskTop);
+
+//Waiting for Enter
+Console.ReadLine();
+
+
+/* Enumeration:
+ * 
+ *  Pay special attention to the way you have specified that you are interested in the desktop.
+    The value of Desktop is one of the values of an enumeration called Environment.Special
+    Folder.
+    Whenever Visual Studio wants you to enter an enumeration’s value, it usually offers
+    you a corresponding enumeration. In this case, when you choose GetFolderPath from
+    IntelliSense and type an open parenthesis afterward, the Environment.SpecialFolder
+    enumeration immediately pops up.
+ */ 
